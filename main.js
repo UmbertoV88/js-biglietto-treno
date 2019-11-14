e20136743e12ca3536665c3f87c297c0f7ba2a56
UmbertoV <umberto.ottone@gmail.com>
2019-11-14 16:25:57 +0100

    Applicato costo biglietto per KM. inizio all'applcazione degli sconti

---
+++ main.js
@@ -0,0 +1,12 @@
+// costo per km
+var costokm = parseFloat("0.21");
+console.log(costokm + "€/km");
+
+//Km da percorrere
+var km = prompt("Quanti Km devi percorrere?");
+console.log(km);
+//calcolo costo biglietto per km da percorrere
+var costobiglietto = (km * "0.21");
+console.log(costobiglietto);
+
+//
