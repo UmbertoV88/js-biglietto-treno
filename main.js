95277a5a73a08ff58b744bce6bb46b97a17f01d4
UmbertoV <umberto.ottone@gmail.com>
2019-11-15 10:32:12 +0100

    Pulizia foglio di lavoro

--- main.js
+++ main.js
@@ -1,24 +1,23 @@
 // costo per km
-var costokm = parseFloat("0.21");
+var costokm = 0.21;
 console.log("Costo del biglietto per Km = " + costokm + "€/km");
 
 //Km da percorrere
-var km = prompt("Quanti Km devi percorrere?");
+var km = parseInt(prompt("Quanti Km devi percorrere?"));
 console.log("Km da percorrere = " + km + "Km");
 //calcolo costo biglietto per km da percorrere
-var costobiglietto = (km * "0.21");
+var costobiglietto = (km * costokm);
 console.log("Costo del biglietto = " + costobiglietto + "€");
 
-var eta = prompt("Quanti anni hai?");
+var eta = parseInt(prompt("Quanti anni hai?"));
 console.log("Età = " + eta + " anni");
 //Sconto del 20% per i minori di 18anni
 var scontominori = costobiglietto - ((costobiglietto * 20)/100);
-// console.log("Prezzo biglietto per i minori di 18 anni = " + scontominori + "€");
+
 //Sconto del 40% per gli over 65
 var scontover = costobiglietto - ((costobiglietto * 40)/100);
-// console.log("Prezzo biglietto per gli over 65 = " + scontover + "€");
 
-
+// Prezzo del biglietto a seconda dell'età inserita
 if (eta <=17) {
     console.log("Prezzo biglietto per i minori di 18 anni = " + scontominori + "€");
     document.getElementById("costo").innerHTML =  scontominori;
@@ -28,7 +27,7 @@ if (eta <=17) {
 }else if (eta){
     document.getElementById("costo").innerHTML = costobiglietto;
 }
-
+// collegamento con HTML
 document.getElementById("row").setAttribute("class", "visible");
 document.getElementById("km").innerHTML = km;
 document.getElementById("eta").innerHTML = eta;
